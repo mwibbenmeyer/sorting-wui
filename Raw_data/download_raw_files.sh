5f3472098a40304bdf195a69ee7eb252336ac51c
Connor <Connor@lennonkeegan.com>
2021-05-20 09:02:22 -0700

    update with script

---
+++ Raw_data/download_raw_files.sh
@@ -0,0 +1,20 @@
+# Download some files
+curl http://silvis.forest.wisc.edu/GeoData/WUI_cp12/zip/fgdb/CONUS_wui_cp12_fgdb.zip
+
+curl https://www.fs.usda.gov/rds/archive/products/RDS-2015-0047-2/RDS-2015-0047-2.zip
+
+curl https://www.fs.usda.gov/rds/archive/products/RDS-2015-0047/RDS-2015-0047.zip
+
+curl https://www.fs.usda.gov/rds/archive/products/RDS-2015-0045/RDS-2015-0045.zip
+
+#NLCD all years
+curl https://s3-us-west-2.amazonaws.com/mrlc/NLCD_Land_Cover_L48_20190424_full_zip.zip
+
+#VHFHSZ files
+
+#SRA
+curl https://osfm.fire.ca.gov/media/7302/fhszssn.zip
+
+#### go here for LRA VHFHSZ files https://osfm.fire.ca.gov/divisions/wildfire-planning-engineering/wildland-hazards-building-codes/fire-hazard-severity-zones-maps/
+
+#### See HMDA download for a special download procedure for those files
